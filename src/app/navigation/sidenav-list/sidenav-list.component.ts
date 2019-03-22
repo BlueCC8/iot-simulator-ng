6cec6ae00b1f84e16c2d9580234a246218b85176
Cristian Cernat <cristian.cernat97@gmail.com>
2019-03-22 11:28:10 +0000

    Implemented the side nav menu list :sparkles:
    -- Added the auth service to check whether the user is authenticated or not;
    -- Styling fixes;

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AuthService } from '../header/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+
+  private authListenerSubs = new Subscription();
   @Output() sidenavClose = new EventEmitter();
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {}
-
-  public onSidenavClose() {
+  ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuthenticated();
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+  onLogout() {
     this.sidenavClose.emit();
+    this.authService.logout();
+  }
+  onSidenavClose() {
+    this.sidenavClose.emit();
+  }
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
   }
 }
