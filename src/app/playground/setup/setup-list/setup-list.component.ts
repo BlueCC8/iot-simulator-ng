7eb3cb5edbddadaa3aefbd8f237b4b33616042ae
Cristian Cernat <cristian.cernat97@gmail.com>
2019-04-12 17:31:10 +0100

    Fix accordion setup and filter-list component :bug:

--- src/app/playground/setup/setup-list/setup-list.component.ts
+++ src/app/playground/setup/setup-list/setup-list.component.ts
@@ -21,7 +21,7 @@ export class SetupListComponent implements OnInit, OnDestroy {
   username: string;
   isLoading = false;
   totalSetups = 0;
-  setupsPerPage = 2;
+  setupsPerPage = 5;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   private isPopulated = true;
