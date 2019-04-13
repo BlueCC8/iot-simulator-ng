4027e757916a5858e1c77b10c18cdaea898eafa6
Cristian Cernat <cristian.cernat97@gmail.com>
2019-04-13 09:46:49 +0100

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyDKPNOo8tC5G73-xOph85ongd4r-eNjzH0',
-    authDomain: 'iot-simulator-9e065.firebaseapp.com',
-    databaseURL: 'https://iot-simulator-9e065.firebaseio.com',
-    projectId: 'iot-simulator-9e065',
-    storageBucket: '',
-    messagingSenderId: '49148556869'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3050/api',
-  firebase: {
-    apiKey: 'AIzaSyDKPNOo8tC5G73-xOph85ongd4r-eNjzH0',
-    authDomain: 'iot-simulator-9e065.firebaseapp.com',
-    databaseURL: 'https://iot-simulator-9e065.firebaseio.com',
-    projectId: 'iot-simulator-9e065',
-    storageBucket: '',
-    messagingSenderId: '49148556869'
-  }
+  apiUrl: 'http://localhost:3050/api'
 };
 
 /*
